79f14b3288da2993affced45515e47ca39f7ffd0
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-02 09:20:37 -0700

    implemented venipede line pretesting

--- PokeModCode/Cards/Uncommon/Scolipede.cs
+++ PokeModCode/Cards/Uncommon/Scolipede.cs
@@ -1,23 +1,61 @@
-using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class Scolipede() : CustomCardModel(int, CardType.Attack,
-    CardRarity.Basic, TargetType.Self)
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Scolipede() : PokeModCard(2, CardType.Attack,
+    CardRarity.Event, TargetType.AllEnemies)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
-
+    protected override bool ShouldGlowGoldInternal => base.CombatState?.HittableEnemies.Any((Creature e) => e.HasPower<PoisonPower>()) ?? false;
+    
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<PoisonPower>()
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(12m, ValueProp.Move),
+        new PowerVar<PoisonPower>(3m)
+    ];
+    
+    
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
-        CardPlay play)
+        CardPlay cardPlay)
     {
-        
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        await PowerCmd.Apply<PoisonPower>(choiceContext, cardPlay.Target, base.DynamicVars.Poison.BaseValue, base.Owner.Creature, this);
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).WithHitCount(1).FromCard(this)
+            .TargetingAllOpponents(base.CombatState)
+            .WithHitFx("vfx/vfx_giant_horizontal_slash")
+            .Execute(choiceContext);
     }
 
     protected override void OnUpgrade()
     {
-
+        base.DynamicVars.Damage.UpgradeValueBy(4m);
     }
 }
--- PokeModCode/Cards/Uncommon/Venipide.cs
+++ PokeModCode/Cards/Uncommon/Venipide.cs
@@ -1,6 +1,58 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class Venipide
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Venipide() : PokeModCard(1, CardType.Attack,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
+    protected override bool ShouldGlowGoldInternal => base.CombatState?.HittableEnemies.Any((Creature e) => e.HasPower<PoisonPower>()) ?? false;
     
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<PoisonPower>()
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(6m, ValueProp.Move)
+    ];
+    
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        int hitCount = ((!cardPlay.Target.HasPower<PoisonPower>()) ? 1 : 2);
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).WithHitCount(hitCount).FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx("vfx/vfx_attack_blunt", null, "heavy_attack.mp3")
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(2m);
+    }
 }
