57a848ccedff6bb466038a4f4d0a383180e73479
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-03 13:09:40 -0700

    added karablast evo line

--- PokeModCode/Cards/Uncommon/Karrablast.cs
+++ PokeModCode/Cards/Uncommon/Karrablast.cs
@@ -1,6 +1,27 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PokeMod.PokeModCode.Character;
 
-public class Karrablast
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Karrablast() : PokeModCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
-    
+    public override bool GainsBlock => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(10, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CommonActions.CardBlock(this, play);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3m);
+    }
 }
