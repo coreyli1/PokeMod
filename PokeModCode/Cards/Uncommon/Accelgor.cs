b0ca5aebf883283e479be495fcc9bb3e99f06391
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-04 16:48:24 -0700

    added shelmetpower which acts the same as Burst and added and tested shelmet and accelgor

--- PokeModCode/Cards/Uncommon/Accelgor.cs
+++ PokeModCode/Cards/Uncommon/Accelgor.cs
@@ -1,6 +1,41 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PokeMod.PokeModCode.Character;
 
-public class Accelgor
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Accelgor() : PokeModCard(1, CardType.Skill, CardRarity.Event, TargetType.Self)
 {
     
+    
+    private const string _skills = "Skills";
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];    
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DynamicVar("Skills", 2m)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<ShelmetPower>(choiceContext, base.Owner.Creature, base.DynamicVars["Skills"].BaseValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
 }
--- PokeModCode/Cards/Uncommon/Shelmet.cs
+++ PokeModCode/Cards/Uncommon/Shelmet.cs
@@ -1,6 +1,39 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PokeMod.PokeModCode.Character;
 
-public class Shelmet
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Shelmet() : PokeModCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
-    
+    private const string _skills = "Skills";
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+        CardKeyword.Exhaust
+    ];    
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DynamicVar("Skills", 1m)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        await PowerCmd.Apply<ShelmetPower>(choiceContext, base.Owner.Creature, base.DynamicVars["Skills"].BaseValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
 }
