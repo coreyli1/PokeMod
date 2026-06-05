3c56df00a950e1ee2328d910d886f78a47737b0a
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-04 22:53:56 -0700

    implement and tested nincada line

--- PokeModCode/Cards/Uncommon/Nincada.cs
+++ PokeModCode/Cards/Uncommon/Nincada.cs
@@ -1,6 +1,56 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using PokeMod.PokeModCode.Character;
 
-public class Nincada
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Nincada() : PokeModCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
+
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new CardsVar(2)
+    ];
     
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+    [
+        HoverTipFactory.FromPower<PlatingPower>(),
+        HoverTipFactory.FromKeyword(PokeModCode.Keywords.Evolve)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CardPileCmd.Draw(choiceContext, base.DynamicVars.Cards.BaseValue, base.Owner);
+        
+        // if plating exists 
+        /*if (play.Target.HasPower<PlatingPower>())
+        {
+            
+        }*/
+        var amt = Owner.Creature.GetPowerAmount<PlatingPower>()*2;
+        await PowerCmd.Apply<PlatingPower>(choiceContext, base.Owner.Creature, amt, base.Owner.Creature, this);
+        
+        //need a power that takes the current plating if it exists and doubles it
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.EnergyCost.UpgradeBy(-1);
+    }
 }
