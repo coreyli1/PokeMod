4b946670592089cf79a5e535b4706015aee85011
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-06 17:12:49 -0700

    added scyther and scyther power, still working on making it work exactly how i want it to

---
+++ PokeModCode/Powers/ScytherPower.cs
@@ -0,0 +1,46 @@
+using BaseLib.Abstracts;
+using System.Linq;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+
+
+namespace PokeMod.PokeModCode.Powers;
+
+
+
+public sealed class ScytherPower : CustomPowerModel
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override int ModifyCardPlayCount(CardModel card, Creature? target, int playCount)
+    {
+        if (card.Owner.Creature != base.Owner)
+        {
+            return playCount;
+        }
+        int num = CombatManager.Instance.History.CardPlaysStarted.Count((CardPlayStartedEntry e) => e.Actor == base.Owner && e.CardPlay.IsFirstInSeries && e.HappenedThisTurn(base.CombatState));
+        if (num >= base.Amount)
+        {
+            return playCount;
+        }
+        if (card.Type != CardType.Attack)
+        {
+            return playCount;
+        }
+        return playCount + 1;
+    }
+
+    public override Task AfterModifyingCardPlayCount(CardModel card)
+    {
+        Flash();
+        return Task.CompletedTask;
+    }
+}
