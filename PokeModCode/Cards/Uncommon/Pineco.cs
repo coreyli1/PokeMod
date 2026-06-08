b2837a076c9041592e433f9d2155026bf58a679f
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-07 18:38:59 -0700

    added pineco

--- PokeModCode/Cards/Uncommon/Pineco.cs
+++ PokeModCode/Cards/Uncommon/Pineco.cs
@@ -1,6 +1,45 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class Pineco
+namespace PokeMod.PokeModCode.Cards;
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Pineco() : PokeModCard(1, CardType.Skill,
+    CardRarity.Uncommon, TargetType.Self)
 {
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
     
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<PlatingPower>()
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<PlatingPower>(4m)
+    ];
+    
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<PlatingPower>(choiceContext, base.Owner.Creature, base.DynamicVars["PlatingPower"].BaseValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["PlatingPower"].UpgradeValueBy(2m);
+    }
 }
