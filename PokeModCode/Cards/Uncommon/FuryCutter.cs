10b7419b87eca546574a3a7feac762a21b51f9fe
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-08 12:24:51 -0700

    added fury cutter and lunge and tested

--- PokeModCode/Cards/Uncommon/FuryCutter.cs
+++ PokeModCode/Cards/Uncommon/FuryCutter.cs
@@ -1,6 +1,64 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class FuryCutter
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class FuryCutter() : PokeModCard(1,CardType.Attack,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
     
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+
+        // CardKeyword.Retain
+    ];    
+    
+    private const string _increaseKey = "Increase";
+    
+    private decimal _extraDamageFromPlays;
+    
+    private decimal ExtraDamageFromPlays
+    {
+        get
+        {
+            return _extraDamageFromPlays;
+        }
+        set
+        {
+            AssertMutable();
+            _extraDamageFromPlays = value;
+        }
+    }
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(3m, ValueProp.Move),
+        new DynamicVar("Increase", 2m)
+        
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+
+        ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).WithHitCount(2).FromCard(this)
+            .Targeting(play.Target)
+            .Execute(choiceContext);
+        
+        base.DynamicVars.Damage.BaseValue += base.DynamicVars["Increase"].BaseValue;
+        ExtraDamageFromPlays += base.DynamicVars["Increase"].BaseValue;
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["Increase"].UpgradeValueBy(4m);
+    }
 }
--- PokeModCode/Cards/Uncommon/Lunge.cs
+++ PokeModCode/Cards/Uncommon/Lunge.cs
@@ -1,6 +1,49 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PokeMod.PokeModCode.Character;
 
-public class Lunge
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Lunge() : PokeModCard(2,CardType.Attack,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
     
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<StrengthPower>()
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(18m, ValueProp.Move),
+        new DynamicVar("StrengthLoss", 1m)
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+
+        ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+        await PowerCmd.Apply<StrengthPower>(choiceContext, play.Target, -base.DynamicVars["StrengthLoss"].BaseValue, base.Owner.Creature, this);
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["StrengthLoss"].UpgradeValueBy(2m);
+    }
 }
