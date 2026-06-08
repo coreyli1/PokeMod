e50f7fd3faec5d1d0e85726e537c9219146a3583
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-08 16:27:42 -0700

    added acupressure and StunSpore.cs
    accupressure needs work adding a new power but im too lazy right now so i just added it as a copy of debilitate

--- PokeModCode/Cards/Uncommon/Acupressure.cs
+++ PokeModCode/Cards/Uncommon/Acupressure.cs
@@ -1,6 +1,52 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using PokeMod.PokeModCode.Character;
 
-public class Acupressure
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Accupressure() : PokeModCard(1, CardType.Attack,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<DebilitatePower>(),
+
+    ];
     
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(10m, ValueProp.Move),
+        new PowerVar<DebilitatePower>(2m)
+    ];
+    
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(play.Target)
+            .WithHitFx("vfx/vfx_attack_slash")
+            .Execute(choiceContext);
+        await PowerCmd.Apply<DebilitatePower>(choiceContext, play.Target, base.DynamicVars["DebilitatePower"].BaseValue, base.Owner.Creature, this);
+    }
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(2m);
+        base.DynamicVars["DebilitatePower"].UpgradeValueBy(1m);
+    }
 }
+
--- PokeModCode/Cards/Uncommon/StunSpore.cs
+++ PokeModCode/Cards/Uncommon/StunSpore.cs
@@ -1,6 +1,52 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using PokeMod.PokeModCode.Character;
 
-public class StunSpore
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class StunSpore() : PokeModCard(1, CardType.Attack,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];    
     
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+
+    ];
+    
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull(play.Target, "cardPlay.Target");
+        await CreatureCmd.Stun(play.Target);
+        
+        
+    }
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
 }
