597b746f004f1ed041e2feb4165f8d1a61fa1c51
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-09 10:49:22 -0700

    added StruggleBug.cs

--- PokeModCode/Cards/Uncommon/StruggleBug.cs
+++ PokeModCode/Cards/Uncommon/StruggleBug.cs
@@ -1,6 +1,51 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class StruggleBug
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class StruggleBug() : PokeModCard(1,CardType.Skill,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
     
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<StrengthPower>(),
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DynamicVar("EnemyStrengthLoss", 2m),
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull(play.Target, "play.Target");
+        foreach (Creature hittableEnemy in base.CombatState.HittableEnemies)
+        {
+            await PowerCmd.Apply<StrengthPower>(choiceContext, play.Target, -base.DynamicVars["EnemyStrengthLoss"].BaseValue, base.Owner.Creature, this);
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["EnemyStrengthLoss"].UpgradeValueBy(1m);
+    }
 }
