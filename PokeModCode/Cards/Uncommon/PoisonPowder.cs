66e3e73b279fae66fed58717c7c9f3f8d2ec44d0
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-08 21:06:27 -0700

    implemented PoisonPowder.cs

--- PokeModCode/Cards/Uncommon/PoisonPowder.cs
+++ PokeModCode/Cards/Uncommon/PoisonPowder.cs
@@ -1,23 +1,52 @@
-using BaseLib.Abstracts;
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class PoisonPowder() : CustomCardModel(int, CardType.Attack,
-    CardRarity.Basic, TargetType.Self)
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class PoisonPowder() : PokeModCard(2,CardType.Skill,
+    CardRarity.Uncommon, TargetType.AllEnemies)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
 
-    protected override async Task OnPlay(
-        PlayerChoiceContext choiceContext,
-        CardPlay play)
+        CardKeyword.Exhaust
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<PoisonPower>(4m),
+        new PowerVar<WeakPower>(2)
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
     {
-        
+        foreach (Creature hittableEnemy in base.CombatState.HittableEnemies)
+        {
+            await PowerCmd.Apply<WeakPower>(choiceContext, hittableEnemy, DynamicVars.Weak.BaseValue, Owner.Creature, this);
+            await PowerCmd.Apply<PoisonPower>(choiceContext, hittableEnemy, base.DynamicVars.Poison.BaseValue, base.Owner.Creature, this);
+        }
     }
 
     protected override void OnUpgrade()
     {
-
+        base.EnergyCost.UpgradeBy(-1);
     }
 }
