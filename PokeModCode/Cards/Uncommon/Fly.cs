1e7f3c2a77283fc9ad3b7b25d17027ea347ecd97
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-08 21:10:08 -0700

    implemented Fly

--- PokeModCode/Cards/Uncommon/Fly.cs
+++ PokeModCode/Cards/Uncommon/Fly.cs
@@ -1,23 +1,48 @@
-using BaseLib.Abstracts;
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class Fly() : CustomCardModel(int, CardType.Attack,
-    CardRarity.Basic, TargetType.Self)
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Fly() : PokeModCard(1, CardType.Skill,
+    CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
 
-    protected override async Task OnPlay(
-        PlayerChoiceContext choiceContext,
-        CardPlay play)
+
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new CardsVar(3),
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
     {
-        
+        await CardPileCmd.Draw(choiceContext, base.DynamicVars.Cards.BaseValue, base.Owner);
+        await PowerCmd.Apply<DrawCardsNextTurnPower>(choiceContext, base.Owner.Creature, base.DynamicVars.Cards.BaseValue, base.Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-
+        base.DynamicVars.Cards.UpgradeValueBy(1);
     }
 }
