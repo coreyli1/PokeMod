240a9e9d7e47139b505146b72058513eea25ea82
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-08 21:00:54 -0700

    added pinsir

--- PokeModCode/Cards/Uncommon/Pinsir.cs
+++ PokeModCode/Cards/Uncommon/Pinsir.cs
@@ -1,6 +1,53 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using PokeMod.PokeModCode.Character;
 
-public class Pinsir
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Pinsir() : PokeModCard(1, CardType.Skill,
+    CardRarity.Uncommon, TargetType.AnyEnemy)
 {
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+    ];
     
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(8, ValueProp.Move),
+        new CardsVar(3)
+    ];
+    
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        ArgumentNullException.ThrowIfNull(play.Target, "play.Target");
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).Targeting(play.Target)
+            .Execute(choiceContext);
+        await CardPileCmd.Draw(choiceContext, base.DynamicVars.Cards.BaseValue, base.Owner);
+    }
+    
+    protected override void OnUpgrade()
+    {
+
+        DynamicVars.Damage.UpgradeValueBy(4m);
+
+    }
+
+
 }
