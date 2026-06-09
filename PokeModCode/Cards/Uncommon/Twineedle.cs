a73861ea563cd7e98609a4f0c7fea6cc4a0ac011
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-08 21:17:57 -0700

    added Twineedle.cs

--- PokeModCode/Cards/Uncommon/Twineedle.cs
+++ PokeModCode/Cards/Uncommon/Twineedle.cs
@@ -1,23 +1,62 @@
-using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using PokeMod.PokeModCode.Character;
 
-public class Twineedle() : CustomCardModel(int, CardType.Attack,
-    CardRarity.Basic, TargetType.Self)
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Twineedle() : PokeModCard(1, CardType.Skill,
+    CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
 
-    protected override async Task OnPlay(
-        PlayerChoiceContext choiceContext,
-        CardPlay play)
+
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new CardsVar(1),
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
     {
-        
+        CardModel selection = (await CardSelectCmd.FromHand(prefs: new CardSelectorPrefs(base.SelectionScreenPrompt, 1), context: choiceContext, player: base.Owner, filter: delegate(CardModel c)
+        {
+            CardType type = c.Type;
+            return (type == CardType.Attack || type == CardType.Power) ? true : false;
+        }, source: this)).FirstOrDefault();
+        if (selection != null)
+        {
+            for (int i = 0; i < base.DynamicVars.Cards.IntValue; i++)
+            {
+                CardModel card = selection.CreateClone();
+                await CardPileCmd.AddGeneratedCardToCombat(card, PileType.Hand, base.Owner);
+            }
+        }
     }
 
     protected override void OnUpgrade()
     {
-
+        base.DynamicVars.Cards.UpgradeValueBy(1);
     }
 }
