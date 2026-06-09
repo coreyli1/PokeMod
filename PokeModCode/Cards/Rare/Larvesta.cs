be27dc6037de8d780c47c19b985d60e5f5d123e1
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-09 11:03:02 -0700

    added larvesta line pre testing

--- PokeModCode/Cards/Rare/Larvesta.cs
+++ PokeModCode/Cards/Rare/Larvesta.cs
@@ -1,6 +1,52 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PokeMod.PokeModCode.Character;
 
-public class Larvesta
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Larvesta() : PokeModCard(2,CardType.Attack,
+    CardRarity.Rare, TargetType.AllEnemies)
 {
+    
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(14m, ValueProp.Move),
+        new DynamicVar("DexterityLoss", 2m)
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await PowerCmd.Apply<DexterityPower>(choiceContext, base.Owner.Creature, -base.DynamicVars.Dexterity.BaseValue, base.Owner.Creature, this);
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).TargetingAllOpponents(base.CombatState)
+            .WithHitCount(2)
+            .Execute(choiceContext);
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(2m);
+    }
     
 }
--- PokeModCode/Cards/Rare/Volcarona.cs
+++ PokeModCode/Cards/Rare/Volcarona.cs
@@ -1,6 +1,54 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PokeMod.PokeModCode.Character;
 
-public class Volcarona
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Volcarona() : PokeModCard(2,CardType.Attack,
+    CardRarity.Event, TargetType.AllEnemies)
 {
+    
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+
+    ];    
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(24m, ValueProp.Move),
+        new DynamicVar("DexterityLoss", 2m)
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await PowerCmd.Apply<DexterityPower>(choiceContext, base.Owner.Creature, -base.DynamicVars.Dexterity.BaseValue, base.Owner.Creature, this);
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).TargetingAllOpponents(base.CombatState)
+            .WithHitCount(2)
+            .Execute(choiceContext);
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(4m);
+        base.DynamicVars["DexterityLoss"].UpgradeValueBy(-1m);
+    }
     
 }
