fc36b92c03e97a1f16ce8a74a4e7bd9ef9f1c599
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-09 10:55:17 -0700

    added defend order

--- PokeModCode/Cards/Uncommon/DefendOrder.cs
+++ PokeModCode/Cards/Uncommon/DefendOrder.cs
@@ -1,23 +1,54 @@
-using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using PokeMod.PokeModCode.Character;
 
-public class DefendOrder() : CustomCardModel(int, CardType.Attack,
-    CardRarity.Basic, TargetType.Self)
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class DefendOrder() : PokeModCard(2, CardType.Power,
+    CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
 
+    ];    
+    
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+        HoverTipFactory.FromPower<DexterityPower>()
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<DexterityPower>(3m)
+    ];
+
+    
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
-        CardPlay play)
+        CardPlay cardPlay)
     {
-        
+        await PowerCmd.Apply<DexterityPower>(choiceContext, base.Owner.Creature, base.DynamicVars.Dexterity.BaseValue, base.Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-
+        base.DynamicVars.Dexterity.UpgradeValueBy(2);
     }
 }
