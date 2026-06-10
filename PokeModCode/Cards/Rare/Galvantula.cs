79cc8eb1b3ade4242f4cd451a7b6b96474004014
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-10 08:55:08 -0700

    added joltik line pre testing

--- PokeModCode/Cards/Rare/Galvantula.cs
+++ PokeModCode/Cards/Rare/Galvantula.cs
@@ -1,6 +1,51 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PokeMod.PokeModCode.Character;
 
-public class Galvantula
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Galvantula() : PokeModCard(2,CardType.Attack,
+    CardRarity.Event, TargetType.AnyEnemy)
 {
+    
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];    
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(25m, ValueProp.Move),
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+        await CreatureCmd.Stun(play.Target);
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(5m);
+    }
     
 }
--- PokeModCode/Cards/Rare/Joltik.cs
+++ PokeModCode/Cards/Rare/Joltik.cs
@@ -1,6 +1,51 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PokeMod.PokeModCode.Character;
 
-public class Joltik
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Joltik() : PokeModCard(2,CardType.Attack,
+    CardRarity.Rare, TargetType.AnyEnemy)
 {
+    
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+        CardKeyword.Exhaust
+    ];    
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(20m, ValueProp.Move),
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+        await CreatureCmd.Stun(play.Target);
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars.Damage.UpgradeValueBy(5m);
+    }
     
 }
