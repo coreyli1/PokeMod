01dd0f328910ef14cd80a8459185bacf661c13ab
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-10 09:55:32 -0700

    add crustle and dwebble line pre testing

--- PokeModCode/Cards/Rare/Crustle.cs
+++ PokeModCode/Cards/Rare/Crustle.cs
@@ -1,6 +1,54 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class Crustle
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Crustle() : PokeModCard(1, CardType.Skill,
+    CardRarity.Common, TargetType.Self)
 {
+    public const int incrementAmount = 5;
+
+    private const string _incrementAmountKey = "IncrementAmount";
     
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+    
+
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<RollingBoulderPower>(10m),
+        new DynamicVar("IncrementAmount", 10m)
+    ];
+    
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<RollingBoulderPower>(choiceContext, base.Owner.Creature, base.DynamicVars["RollingBoulderPower"].IntValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["RollingBoulderPower"].UpgradeValueBy(5m);
+        AddKeyword(CardKeyword.Innate);
+    }
 }
--- PokeModCode/Cards/Rare/Dwebble.cs
+++ PokeModCode/Cards/Rare/Dwebble.cs
@@ -1,6 +1,52 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class Dwebble
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class Dwebble() : PokeModCard(1, CardType.Skill,
+    CardRarity.Common, TargetType.Self)
 {
+    public const int incrementAmount = 5;
+
+    private const string _incrementAmountKey = "IncrementAmount";
     
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+    ];    
+    
+
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [
+
+    ];
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<RollingBoulderPower>(5m),
+        new DynamicVar("IncrementAmount", 5m)
+    ];
+    
+    
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<RollingBoulderPower>(choiceContext, base.Owner.Creature, base.DynamicVars["RollingBoulderPower"].IntValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["RollingBoulderPower"].UpgradeValueBy(5m);
+    }
 }
