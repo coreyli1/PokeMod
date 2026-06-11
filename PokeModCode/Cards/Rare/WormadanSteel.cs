f4d17bf67f13d9fa6eefc11f279f1545fae1dad3
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-10 20:42:22 -0700

    add burmy steel line pre testing

--- PokeModCode/Cards/Rare/WormadanSteel.cs
+++ PokeModCode/Cards/Rare/WormadanSteel.cs
@@ -1,6 +1,59 @@
-namespace PokeMod.PokeModCode.Cards.Rare;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PokeMod.PokeModCode.Character;
 
-public class WormadanSteel
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class WormadanSteel() : PokeModCard(2,CardType.Attack,
+    CardRarity.Event, TargetType.AnyEnemy)
 {
+    
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+
+    ];    
+    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new CalculationBaseVar(4m),
+        new ExtraDamageVar(1m),
+        new CalculatedDamageVar(ValueProp.Move).WithMultiplier((CardModel card, Creature? _) =>
+        {
+            return card.Owner.Creature.GetPowerAmount<PlatingPower>();
+        })
+    ];
+    
+    
+    protected override async Task OnPlay( PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await DamageCmd.Attack(base.DynamicVars.Damage.BaseValue).FromCard(this).TargetingAllOpponents(base.CombatState)
+            .WithHitCount(3).Execute(choiceContext);
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.EnergyCost.UpgradeBy(-1);
+
+    }
     
 }
