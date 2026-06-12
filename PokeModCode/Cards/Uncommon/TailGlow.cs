59cc0bb44ba7025fe2fce3f9ff0b57e5f5951cb9
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-11 17:26:12 -0700

    add TailGlow.cs

--- PokeModCode/Cards/Uncommon/TailGlow.cs
+++ PokeModCode/Cards/Uncommon/TailGlow.cs
@@ -1,6 +1,44 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using PokeMod.PokeModCode.Character;
+using PokeMod.PokeModCode.Powers;
 
-public class TailGlow
+namespace PokeMod.PokeModCode.Cards;
+
+
+
+
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class TailGlow() : PokeModCard(2, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
 {
-    
+    private const string _powers = "Powers";
+    protected override HashSet<CardTag> CanonicalTags => new HashSet<CardTag> { PokeModCode.Tags.Evolve };
+    //add evolve keyword
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        PokeModCode.Keywords.Evolve,
+        CardKeyword.Exhaust
+    ];    
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DynamicVar("Powers", 1m),
+        new DamageVar(13, ValueProp.Move)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        await CommonActions.CardAttack(this, play).Execute(choiceContext);
+        await PowerCmd.Apply<TailGlowPower>(choiceContext, base.Owner.Creature, base.DynamicVars["Powers"].BaseValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.EnergyCost.UpgradeBy(-1);
+    }
 }
