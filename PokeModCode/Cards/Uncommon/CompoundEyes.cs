6ff494024c4895c1feeb96372e6c1a9431e74d5c
coreyli1 <44040313+coreyli1@users.noreply.github.com>
2026-06-13 12:26:02 -0700

    added compound eyes pretesting

--- PokeModCode/Cards/Uncommon/CompoundEyes.cs
+++ PokeModCode/Cards/Uncommon/CompoundEyes.cs
@@ -1,6 +1,43 @@
-namespace PokeMod.PokeModCode.Cards.Uncommon;
+using BaseLib.Utils;
+using System.Linq;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using MegaCrit.Sts2.Core.HoverTips;
+using PokeMod.PokeModCode.Character;
 
-public class CompoundEyes
+namespace PokeMod.PokeModCode.Cards;
+
+
+[Pool(typeof(BugCatcherCardPool))]
+public sealed class CompoundEyes() : PokeModCard(1, CardType.Skill, CardRarity.Common, TargetType.AllEnemies)
 {
-    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new EnergyVar(4)
+    ];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
+    {
+        await CreatureCmd.TriggerAnim(base.Owner.Creature, "Cast", base.Owner.Character.CastAnimDelay);
+        await PlayerCmd.GainEnergy(base.DynamicVars.Energy.IntValue, base.Owner);
+        foreach (var enemy in base.CombatState.HittableEnemies)
+        {
+            var debuffs = enemy.Powers
+                .Where(p => p.TypeForCurrentAmount == PowerType.Debuff)
+                .ToList();
+            foreach (var debuff in debuffs)
+            {
+                await PowerCmd.Remove(debuff);
+            }
+        }
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.EnergyCost.UpgradeBy(-1);
+    }
 }
